6bd6c0efc2779433e70d7503cf0b1d2939d04f73
princesv <princeeeee.12@gmail.com>
2020-07-15 16:17:49 +0530

    add track using foreign key method

---
+++ app/src/main/java/com/prince/fbdbsample2/AddTrackActivity.java
@@ -0,0 +1,67 @@
+package com.prince.fbdbsample2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class AddTrackActivity extends AppCompatActivity {
+
+    TextView textViewArtistName;
+    EditText editTextTrackName;
+    SeekBar seekBarRating;
+    Button addTrack;
+    DatabaseReference databaseTracks;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+       setContentView(R.layout.activity_add_track);
+        textViewArtistName=findViewById(R.id.textViewArtistName);
+        editTextTrackName=findViewById(R.id.edittextTrackName);
+        seekBarRating=findViewById(R.id.seekBarRating);
+        addTrack=findViewById(R.id.buttonAddTrack);
+        Intent intent=getIntent();
+        String id=intent.getStringExtra(MainActivity.ARTIST_ID);
+        String name=intent.getStringExtra(MainActivity.ARTIST_NAME);
+        textViewArtistName.setText(name);
+        databaseTracks= FirebaseDatabase.getInstance().getReference("tracks").child(id);
+        addTrack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveTrack();
+            }
+        });
+
+    }
+    public void saveTrack() {
+
+        String trackName = editTextTrackName.getText().toString().trim();
+        int rating = seekBarRating.getProgress();
+
+        if (!TextUtils.isEmpty(trackName)) {
+            String trackId=databaseTracks.push().getKey();
+            Track track=new Track(trackId,trackName,rating);
+            databaseTracks.child(trackId).setValue(track);
+            Toast.makeText(this, "Track saved successfully", Toast.LENGTH_SHORT).show();
+
+
+        } else {
+            Toast.makeText(this, "Track empty", Toast.LENGTH_SHORT).show();
+        }
+
+
+
+    }
+}
--- app/src/main/java/com/prince/fbdbsample2/MainActivity.java
+++ app/src/main/java/com/prince/fbdbsample2/MainActivity.java
@@ -3,10 +3,12 @@ package com.prince.fbdbsample2;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
@@ -23,6 +25,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String ARTIST_NAME="artistname";
+    public static final String ARTIST_ID="artistid";
     EditText editTextName;
     Button buttonAdd;
     Spinner spinnerGenere;
@@ -46,6 +51,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 addArtist();
+            }
+        });
+
+        listViewArtists.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int i, long l) {
+             Artist artist=artistList.get(i);
+                Intent intent=new Intent(getApplicationContext(),AddTrackActivity.class);
+                intent.putExtra(ARTIST_ID,artist.getArtistId());
+                intent.putExtra(ARTIST_NAME,artist.getArtistName());
+                startActivity(intent);
             }
         });
 
